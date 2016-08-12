f94cb33f9a6522f52379e9cbaaa8ba4cc68726c6
Vaibhav Singh <vaibhav1.s@samsung.com>
2016-08-12 16:41:50 +0200

    GPIO: Correcting gpio test for artik 710

--- test/gpio-test.js
+++ test/gpio-test.js
@@ -1,63 +1,66 @@
-var gpio = require('../src/gpio');
+var gpio  = require('../src/gpio');
 var artik = require('../lib/artik-sdk');
+var button, red, green, blue;
 
 const name = artik.get_platform_name();
 
 if(name == 'Artik 520') {
 	console.log('Running GPIO test on Artik 520');
 	const a5 = require('../src/platform/artik520');
-	var red = new gpio(a5.ARTIK_A5_GPIO_XEINT0, 'red', 'out', 'digital', 'none', 0);
-	var green = new gpio(a5.ARTIK_A5_GPIO_XEINT1, 'green', 'out', 'digital', 'none', 0);
-	var blue = new gpio(a5.ARTIK_A5_GPIO_XEINT2, 'blue', 'out', 'digital', 'none', 0);
-	var button = new gpio(a5.ARTIK_A5_GPIO_XEINT3, 'button', 'in' , 'digital', 'both', 0);
-	button.request();
+	red = new gpio(a5.ARTIK_A5_GPIO_XEINT0, 'red', 'out', 'digital', 'none', 0);
+	green = new gpio(a5.ARTIK_A5_GPIO_XEINT1, 'green', 'out', 'digital', 'none', 0);
+	blue = new gpio(a5.ARTIK_A5_GPIO_XEINT2, 'blue', 'out', 'digital', 'none', 0);
+	button = new gpio(a5.ARTIK_A5_GPIO_XEINT3, 'button', 'in' , 'digital', 'both', 0);
 } else if(name == 'Artik 1020') {
 	console.log('Running GPIO test on Artik 1020');
 	const a10 = require('../src/platform/artik1020');
-	var red = new gpio(a10.ARTIK_A10_GPIO_XEINT0, 'red', 'out', 'digital', 'none', 0);
-	var green = new gpio(a10.ARTIK_A10_GPIO_XEINT1, 'green', 'out', 'digital', 'none', 0);
-	var blue = new gpio(a10.ARTIK_A10_GPIO_XEINT2, 'blue', 'out', 'digital', 'none', 0);
-	var button = new gpio(a10.ARTIK_A10_GPIO_XEINT3, 'button', 'in' , 'digital', 'both', 0);
-	button.request();
+	red = new gpio(a10.ARTIK_A10_GPIO_XEINT0, 'red', 'out', 'digital', 'none', 0);
+	green = new gpio(a10.ARTIK_A10_GPIO_XEINT1, 'green', 'out', 'digital', 'none', 0);
+	blue = new gpio(a10.ARTIK_A10_GPIO_XEINT2, 'blue', 'out', 'digital', 'none', 0);
+	button = new gpio(a10.ARTIK_A10_GPIO_XEINT3, 'button', 'in' , 'digital', 'both', 0);
 } else if(name == 'Artik 710') {
 	console.log('Running GPIO test on Artik 710');
 	const a7 = require('../src/platform/artik710');
-	var red = new gpio(a7.ARTIK_A710_GPIO_GPIO0, 'red', 'out', 'digital', 'none', 0);
-	var green = new gpio(a7.ARTIK_A710_GPIO_GPIO1, 'green', 'out', 'digital', 'none', 0);
-	var blue = new gpio(a7.ARTIK_A710_GPIO_GPIO2, 'blue', 'out', 'digital', 'none', 0);
-	var button = new gpio(a7.ARTIK_A710_GPIO_GPIO4, 'button', 'in' , 'digital', 'both', 0);
-	button.request();
+	red = new gpio(a7.ARTIK_A710_GPIO_GPIO0, 'red', 'out', 'digital', 'none', 0);
+	green = new gpio(a7.ARTIK_A710_GPIO_GPIO1, 'green', 'out', 'digital', 'none', 0);
+	blue = new gpio(a7.ARTIK_A710_GPIO_GPIO2, 'blue', 'out', 'digital', 'none', 0);
+	button = new gpio(a7.ARTIK_A710_GPIO_GPIO4, 'button', 'in' , 'digital', 'both', 0);
 }
 
 button.on('changed', function(val) {
 	console.log("Button state: " + val);
 });
 
+button.request();
+
 var color = 0;
+
+console.log('Launch periodic interval');
 
 setInterval(function () {
 
-    red.request();
-    red.write(color & 1);
-    red.release();
+	console.log('Setting color');
 
-    green.request();
-    green.write(color & 2);
-    green.release();
+	red.request();
+	red.write(color & 1);
+	red.release();
 
-    blue.request();
-    blue.write(color & 4);
-    blue.release();
+	green.request();
+	green.write(color & 2);
+	green.release();
 
-    color = (color + 1) % 8;
+	blue.request();
+	blue.write(color & 4);
+	blue.release();
+
+	color = (color + 1) % 8;
 
 },1000);
 
 process.on('SIGINT', function () {
 	console.log('exiting test');
 	button.release();
-    process.exit(0);
+	process.exit(0);
 });
-
 
 
