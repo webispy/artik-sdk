e1b6c838d0977d8e4970576ac914ab29984a3e27
Vaibhav Singh <vaibhav1.s@samsung.com>
2016-10-17 17:25:43 +0200

    Network: Update get ip api
    Signed-off-by: Vaibhav Singh <vaibhav1.s@samsung.com>

--- test/network-test.js
+++ test/network-test.js
@@ -2,7 +2,7 @@ const artik = require('../lib/artik-sdk');
 
 var network = artik.network();
 
-console.log("Your IP is " + network.get_current_ip());
+console.log("Your IP is " + network.get_current_public_ip());
 
 network.get_online_status(function(response) {
 	console.log("Status : " + JSON.parse(response).online_status);
