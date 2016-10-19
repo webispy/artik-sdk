a3e68b2db8ba9f7a9ce6b9997efab034ef23c823
Vaibhav Singh <vaibhav1.s@samsung.com>
2016-10-19 15:59:58 +0200

    Rename test to examples
    Signed-off-by: Vaibhav Singh <vaibhav1.s@samsung.com>

--- test/network-test.js
+++ test/network-test.js
@@ -1,9 +1,35 @@
-const artik = require('../lib/artik-sdk');
+/* Global Include */
+var testCase   = require('mocha').describe;
+var pre        = require('mocha').before;
+var assertions = require('mocha').it;
+var assert     = require('chai').assert;
+var validator  = require('validator');
+var artik      = require('../lib/artik-sdk');
 
-var network = artik.network();
 
-console.log("Your IP is " + network.get_current_public_ip());
+var network;
 
-network.get_online_status(function(response) {
-	console.log("Status : " + JSON.parse(response).online_status);
+testCase('Network', function() {
+	pre(function() {
+		network = artik.network();
+	});
+
+	testCase('#get_online_status()', function() {
+		assertions('Online Status - Should return 1 when it is online', function(done) {
+			network.get_online_status(function(response) {
+				console.log("Response: " + response);
+				assert.equal(JSON.parse(response).online_status, 1);
+				done();
+			});
+		});
+	});
+
+	testCase('#get_current_public_ip()', function() {
+		assertions('Get Current Public Ip - Should return a valid IP', function(done) {
+				var ip = network.get_current_public_ip();
+				console.log("IP Address: " + ip);
+				assert( validator.isIP(ip), true );
+				done();
+			});
+		});
 });
