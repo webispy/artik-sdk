0ee31affa98328352463dc9c10c31e4ee831815e
Gregory Lemercier <g.lemercier@samsung.com>
2016-10-26 03:31:33 -0400

    Revert "Cloud: Fixing segfault on artik710 by using sync operations due to current limitation of artik api"
    This reverts commit 8e159a5962159b9e238475b9cafc4212210f39b0.

--- test/cloud-test.js
+++ test/cloud-test.js
@@ -1,5 +1,3 @@
-/* Cloud API Test Using Sync Calls */
-
 const artik = require('../lib/artik-sdk');
 
 var auth_token = '';
@@ -10,26 +8,46 @@ var action = '';
 
 var cloud = artik.cloud(auth_token);
 
+cloud.get_current_user_profile(function(response) {
+	console.log("Get Current User Profile - response: " + response);
+});
 
-console.log("Get Current User Profile - response: " + cloud.get_current_user_profile());
+cloud.get_device_token(device_id, function(response) {
+	console.log("Get Device Token - response: " + response);
+});
 
-console.log("Get Device Token - response: " + cloud.get_device_token(device_id));
+cloud.get_device(device_id, false, function(response) {
+	console.log("Get Device without properties - response: " + response);
+});
 
-console.log("Get Device without properties - response: " + cloud.get_device(device_id, false));
+cloud.get_device(device_id, true, function(response) {
+	console.log("Get Device with properties - response: " + response);
+});
 
-console.log("Get Device with properties - response: " + cloud.get_device(device_id, true));
+cloud.get_user_device_types(100, false, 0, user_id, function(response) {
+	console.log("Get User Device Types - response: " + response);
+});
 
-console.log("Get User Device Types - response: " + cloud.get_user_device_types(100, false, 0, user_id));
+cloud.get_user_devices(100, false, 0, user_id, function(response) {
+	console.log("Get User Devices without properties - response: " + response);
+});
 
-console.log("Get User Devices without properties - response: " + cloud.get_user_devices(100, false, 0, user_id));
+cloud.get_user_devices(100, true, 0, user_id, function(response) {
+	console.log("Get User Devices with properties - response: " + response);
+});
 
-console.log("Get User Devices with properties - response: " + cloud.get_user_devices(100, true, 0, user_id));
+cloud.send_action(device_id, action, function(response) {
+	console.log("Send action - response: " + response);
+});
 
+cloud.send_message(device_id, message, function(response) {
+	console.log("Send message - response: " + response);
+});
 
-console.log("Send action - response: " + cloud.send_action(device_id, action));
+cloud.update_device_token(device_id, function(response) {
+	console.log("Update Device Token - response: " + response);
+});
 
-console.log("Send message - response: " + cloud.send_message(device_id, message));
-
-console.log("Update Device Token - response: " + cloud.update_device_token(device_id));
-
-console.log("Delete Device Token - response: " + cloud.delete_device_token(device_id));
+cloud.delete_device_token(device_id, function(response) {
+	console.log("Delete Device Token - response: " + response);
+});
