80c56b177b62980d3636dd7f765c015e26d19ad7
Vaibhav Singh <vaibhav1.s@samsung.com>
2016-10-28 11:52:38 +0200

    MQTT: Fix on started event test
    Signed-off-by: Vaibhav Singh <vaibhav1.s@samsung.com>

--- test/mqtt-test.js
+++ test/mqtt-test.js
@@ -12,7 +12,7 @@ var artik      = require('../lib/artik-sdk');
 
 
 /* Test Specific Includes */
-var mqtt             = new(require('../src/mqtt.js'))();
+var mqtt;
 var client_id        = 'artik_mqtt_client';
 var host             = 'api.artik.cloud';
 var port             = 8883;
@@ -26,136 +26,137 @@ var ca_cert          = '/etc/ssl/certs/ca-bundle.crt';
 testCase('MQTT', function() {
 
 
-	pre(function() {
-	});
+    pre(function() {
+    });
 
-	testCase('#on(started)', function() {
+    testCase('#on(started)', function() {
 
-		assertions('Return callback event when the mqtt module is started', function(done) {
+        assertions('Return callback event when the mqtt module is started', function(done) {
 
-			mqtt.on('started', function() {
-				done();
-			});
+            mqtt.on('started', function() {
+                done();
+            });
 
-		});
-	});
+            mqtt = new(require('../src/mqtt.js'))();
+        });
+    });
 
-	testCase('#create_client(), connect_server(), on(connected)', function() {
+    testCase('#create_client(), connect_server(), on(connected)', function() {
 
-		assertions('Return callback event when the mqtt client is connected', function(done) {
-
-            if (!akc_device_id || !akc_device_token || !akc_device_id.length || !akc_device_token.length)
-                this.skip();
-
-			mqtt.on('connected', function(result) {
-				console.log('on connected: ' + result);
-				assert.equal(result, "CONNECTED");
-				done();
-			});
-
-			mqtt.create_client(client_id, akc_device_id, akc_device_token,
-							   false, 0, true);
-			mqtt.tls_set(ca_cert);
-			mqtt.connect_server(host, port);
-		});
-
-	});
-
-	testCase('#subscribe(), on(subscribed)', function() {
-
-		assertions('Return callback event when the mqtt client subscribes to a topic', function(done) {
+        assertions('Return callback event when the mqtt client is connected', function(done) {
 
             if (!akc_device_id || !akc_device_token || !akc_device_id.length || !akc_device_token.length)
                 this.skip();
 
-			mqtt.on('subscribed', function(mid) {
-				console.log('on subscribed: ' + mid);
-				assert.isNotNull(mid);
-				done();
-			});
+            mqtt.on('connected', function(result) {
+                console.log('on connected: ' + result);
+                assert.equal(result, "CONNECTED");
+                done();
+            });
 
-			/* Subscribe to receive actions ¨*/
-			mqtt.subscribe(0, '/v1.1/actions/' + akc_device_id);
+            mqtt.create_client(client_id, akc_device_id, akc_device_token,
+                               false, 0, true);
+            mqtt.tls_set(ca_cert);
+            mqtt.connect_server(host, port);
+        });
 
-		});
+    });
 
-	});
+    testCase('#subscribe(), on(subscribed)', function() {
 
-	testCase('#publish(), on(published), on(message)', function() {
-
-		assertions('Return callback event when the mqtt client receives a message', function(done) {
-
-            if (!akc_device_id || !akc_device_token || !akc_device_id.length || !akc_device_token.length)
-                this.skip();
-
-			this.timeout(5000);
-
-			mqtt.on('published', function(mid) {
-				console.log('on published: ' + mid);
-				assert.isNotNull(mid);
-			});
-
-			mqtt.on('message', function(mid, topic, buffer, qos, retain) {
-				console.log('Received message');
-				console.log('\tmid: ' + mid);
-				console.log('\tqos: ' + qos);
-				console.log('\tretain: ' + retain);
-				console.log('\ttopic: ' + topic);
-				console.log('\tdata: ' + buffer);
-				assert.isNotNull(mid);
-				done();
-			});
-
-			/* Publish a message */
-			mqtt.publish(0, false, '/v1.1/messages/' + akc_device_id, new Buffer(akc_msg));
-
-		});
-
-
-	});
-
-	testCase('#unsubscribe(), on(unsubscribed)', function() {
-
-		assertions('Return callback event when the mqtt client unsubscribes to a topic', function(done) {
+        assertions('Return callback event when the mqtt client subscribes to a topic', function(done) {
 
             if (!akc_device_id || !akc_device_token || !akc_device_id.length || !akc_device_token.length)
                 this.skip();
 
-			mqtt.on('unsubscribed', function(mid) {
-				console.log('on unsubscribed: ' + mid);
-				assert.isNotNull(mid);
-				done();
-			});
+            mqtt.on('subscribed', function(mid) {
+                console.log('on subscribed: ' + mid);
+                assert.isNotNull(mid);
+                done();
+            });
 
-			/* Subscribe to receive actions ¨*/
-			mqtt.unsubscribe('/v1.1/actions/' + akc_device_id);
+            /* Subscribe to receive actions ¨*/
+            mqtt.subscribe(0, '/v1.1/actions/' + akc_device_id);
 
-		});
+        });
 
-	});
+    });
 
-	testCase('#disconnect(), on(disconnected)', function() {
+    testCase('#publish(), on(published), on(message)', function() {
 
-		assertions('Return callback event when the mqtt client unsubscribes to a topic', function(done) {
+        assertions('Return callback event when the mqtt client receives a message', function(done) {
 
             if (!akc_device_id || !akc_device_token || !akc_device_id.length || !akc_device_token.length)
                 this.skip();
 
-			mqtt.on('disconnected', function(result) {
-				console.log('on disconnected: ' + result);
-				assert.equal(result, "DISCONNECTED");
-				mqtt.destroy_client();
-				done();
-			});
+            this.timeout(5000);
 
-			/* Subscribe to receive actions ¨*/
-			mqtt.disconnect();
+            mqtt.on('published', function(mid) {
+                console.log('on published: ' + mid);
+                assert.isNotNull(mid);
+            });
 
-		});
+            mqtt.on('message', function(mid, topic, buffer, qos, retain) {
+                console.log('Received message');
+                console.log('\tmid: ' + mid);
+                console.log('\tqos: ' + qos);
+                console.log('\tretain: ' + retain);
+                console.log('\ttopic: ' + topic);
+                console.log('\tdata: ' + buffer);
+                assert.isNotNull(mid);
+                done();
+            });
 
-	});
+            /* Publish a message */
+            mqtt.publish(0, false, '/v1.1/messages/' + akc_device_id, new Buffer(akc_msg));
 
-	post(function() {
-	});
+        });
+
+
+    });
+
+    testCase('#unsubscribe(), on(unsubscribed)', function() {
+
+        assertions('Return callback event when the mqtt client unsubscribes to a topic', function(done) {
+
+            if (!akc_device_id || !akc_device_token || !akc_device_id.length || !akc_device_token.length)
+                this.skip();
+
+            mqtt.on('unsubscribed', function(mid) {
+                console.log('on unsubscribed: ' + mid);
+                assert.isNotNull(mid);
+                done();
+            });
+
+            /* Subscribe to receive actions ¨*/
+            mqtt.unsubscribe('/v1.1/actions/' + akc_device_id);
+
+        });
+
+    });
+
+    testCase('#disconnect(), on(disconnected)', function() {
+
+        assertions('Return callback event when the mqtt client unsubscribes to a topic', function(done) {
+
+            if (!akc_device_id || !akc_device_token || !akc_device_id.length || !akc_device_token.length)
+                this.skip();
+
+            mqtt.on('disconnected', function(result) {
+                console.log('on disconnected: ' + result);
+                assert.equal(result, "DISCONNECTED");
+                mqtt.destroy_client();
+                done();
+            });
+
+            /* Subscribe to receive actions ¨*/
+            mqtt.disconnect();
+
+        });
+
+    });
+
+    post(function() {
+    });
 
 });
