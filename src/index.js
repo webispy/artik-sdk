3e849a18f683eeaea08c44c142fc7803e8a6073a
Gregory Lemercier <g.lemercier@samsung.com>
2017-02-13 08:18:42 -0500

    Update "artik-sdk" module exports
      - Add root functions to module export
      - Add export to A530 platform definitions

--- src/index.js
+++ src/index.js
@@ -2,9 +2,17 @@
 module.exports.artik520 = require('./platform/artik520');
 module.exports.artik710 = require('./platform/artik710');
 module.exports.artik1020 = require('./platform/artik1020');
+module.exports.artik530 = require('./platform/artik530');
 
 /* Addon exports */
 var artik = require('../lib/artik-sdk');
+
+module.exports.get_modules = artik.get_modules;
+module.exports.get_platform = artik.get_platform;
+module.exports.get_platform_name = artik.get_platform_name;
+module.exports.get_device_info = artik.get_device_info;
+module.exports.destroy = artik.destroy;
+
 module.exports.adc = artik.adc;
 module.exports.cloud = artik.cloud;
 module.exports.http = artik.http;
